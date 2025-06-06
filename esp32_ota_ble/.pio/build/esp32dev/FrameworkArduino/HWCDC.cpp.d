41e92beaaea88829d6417d84abc0b4d28dc90134
Sonusubi <sonusubi456@gmail.com>
2025-06-06 21:25:31 +0530

    Remove unnecessary files

--- esp32_ota_ble/.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.o: \
- /home/sonu-s/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
- /home/sonu-s/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/sonu-s/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
